3a2975a0e57401facab921f7e8e072567195cb0d
Michael Cannon <mc@aihr.us>
2014-03-04 15:13:33 +0700

    Initial code

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+
+// Spark Child Theme
+
+/* 
+	NOTE: 
+		get_template_directory_uri(); returns the path to the PARENT theme directory.
+	 	get_stylesheet_directory_uri(); returns the path to this current CHILD theme directory.
+*/
+
+
+// Here is how to enqueue CSS and JS files properly:
+add_action('init', 'my_custom_js_and_css');
+function my_custom_js_and_css() {
+	
+	// Enqueue "style.css" from current Child Theme
+	spark_add_styles(array('my-custom-css' => array('url' => get_stylesheet_directory_uri() . '/style.css')));
+
+	// Enqueue "scripts.js" from current Child Theme
+	spark_add_scripts(array('my-custom-js' => array('url' => get_stylesheet_directory_uri().'/scripts.js', 'depends_on' => array('jquery'))));
+
+}
+
+
+
+// If you want to REMOVE CSS or JS files that are enqueued by the Spark Parent Theme, 
+// then copy/past the content from /core/css-frontend.php to here and then edit the 2 main arrays by removing the unwanted styles/scripts from it.
+
+
+// Tip: You can easily "override" most functions from the Spark Theme, 
+// as most of them check for "if function_exists(...)" before defining the function, 
+// therefor you can copy/past functions (only those with a "function_exists()" check) from the Spark theme and edit them here.
+
+
+
---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+// Put your custom script here!
+
+$(document).ready(function() {
+
+	alert("Welcome to the Spark Child Theme. \nThis theme is optional and is intended for developers. \n\rThe red border at the top, as well as this alert are examples of customization. \n\rYou can remove the red top-border from the \"style.css\" file. \nAnd remove this alert from the \"scripts.js\" file. \n\rBoth of these files are located in the following directory: \n/wp-content/themes/spark-3.5-child/");
+});
